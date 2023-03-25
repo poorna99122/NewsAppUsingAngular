52c7c393c8300c1b5b8815f62bf6266ebfffb9fa
poorna99122 <46306217+poorna99122@users.noreply.github.com>
2023-03-25 18:33:17 +0530

    added menu open and close functionalities to buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,48 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild,ChangeDetectorRef } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { ChangeDetectionStrategy } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'pcrnews';
+  isSideNavVisible : boolean = false; 
   @ViewChild(MatSidenav) sideNav!: MatSidenav;
 
-  constructor(){
-    
+  constructor(private observer : BreakpointObserver, private cdr : ChangeDetectorRef){
+
   }
+  ngAfterViewInit(): void {
+    this.sideNav.opened = true;
+    this.observer.observe(['(max-width:787px)']).subscribe((res)=>{
+        if(res?.matches){
+          this.sideNav.mode = "over";
+          this.sideNav.close();
+        }
+        else {
+          this.sideNav.mode = "side";
+          this.sideNav.open();
+        }
+    })
+    this.cdr.detectChanges();
+  }
+
+
+
+  sideNavOpen(){
+    this.sideNav.open();
+    this.isSideNavVisible = false;
+  }
+
+  sideNavClose(){
+    this.sideNav.close();
+    this.isSideNavVisible = true;
+  }
+
+
+
 }
